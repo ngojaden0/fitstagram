c010aaa5dc140cda8c054053190f0779388c3ff5
Jaden <ngojaden0>
2022-04-18 19:25:15 -0500

    updated posting, merged to master, got firebase working

---
+++ app/src/main/java/com/example/fitstagram/GeneralFeed.java
@@ -0,0 +1,65 @@
+package com.example.fitstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.MessageFormat;
+
+public class GeneralFeed extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance(); //instantiate firestore
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_feed);
+
+        PostButton(); //post button
+        UserProfileButton(); // Justine
+        RankingButton(); // Christian
+
+        TextView ExamplePost = (TextView) findViewById(R.id.ExamplePost);
+
+        //example post
+        post examplePost = new post(123,"hi guys which outfit is best", false, false,10,null);
+
+        //example - add post object to firestore
+        //db.collection("feed").add(examplePost);
+
+        ExamplePost.setText(MessageFormat.format("User: {0}\nDescription: {1}\nTime: {2}", examplePost.getUser_id(), examplePost.getDescription(), examplePost.getTime()));
+    }
+
+    private void UserProfileButton() {
+        /*
+              Initialize a button that will take the user to their profile
+              Create another activity
+              Use Intent Class
+         */
+    }
+
+    private void RankingButton() {
+        /*
+              Initialize a button that will take the user to the current ranking
+              Create another activity
+              Use Intent Class
+         */
+    }
+
+    private void PostButton() {
+        Button postButton = (Button) findViewById(R.id.post_button);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start next activity
+                startActivity(new Intent(GeneralFeed.this, userpost_or_voterpost.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/fitstagram/PostInformation.java
+++ app/src/main/java/com/example/fitstagram/PostInformation.java
@@ -1,14 +1,22 @@
 package com.example.fitstagram;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class PostInformation extends AppCompatActivity {
+    private Object NullPointerException;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +37,23 @@ public class PostInformation extends AppCompatActivity {
         Button postSubmitButton = (Button) findViewById(R.id.submit_post_button);
         EditText description = (EditText) findViewById(R.id.description);
         EditText time = (EditText) findViewById(R.id.time);
+        AlertDialog.Builder builder = new AlertDialog.Builder(PostInformation.this);
         postSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                // error handle no user input
+                Intent intent = new Intent(PostInformation.this,GeneralFeed.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                if(!TextUtils.isEmpty(description.getText().toString()) || !TextUtils.isEmpty(time.getText().toString())) {
+                    db.collection("feed").add(new post(123, description.getText().toString(), false, false, Integer.parseInt(time.getText().toString()), null));
+                    startActivity(intent);
+                }
+                else {
+                    builder.setMessage("Please fill in information");
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
             }
-            //transfer data between activities
-            //in posting branch
         });
     }
 }
