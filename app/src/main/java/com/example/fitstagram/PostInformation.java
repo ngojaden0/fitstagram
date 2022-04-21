6df4ffffa13eaeca6530982488a86e8ff2e37432
ngojaden0 <ngojaden0@github.com>
2022-04-20 20:03:08 -0500

    posting: updated class files, created image handling functions for posts, images are user/post identified and stored in Firebase Storage instead of Firestore

--- app/src/main/java/com/example/fitstagram/PostInformation.java
+++ app/src/main/java/com/example/fitstagram/PostInformation.java
@@ -1,28 +1,82 @@
 package com.example.fitstagram;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.example.fitstagram.databinding.ActivityPostInformationBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class PostInformation extends AppCompatActivity {
     private Object NullPointerException;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    ActivityPostInformationBinding binding;
+    ActivityResultLauncher<String> mTakePhoto;
+    Bitmap photo;
+    Uri file;
+    StorageReference storageRef = storage.getReference(); //created a reference
+    StorageReference pictures;
+    UploadTask uploadTask;
+    InputStream stream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_information);
+        binding = ActivityPostInformationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         PostBackButton();
-        PostSubmitButton();
+        UploadButton();
+        mTakePhoto = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @RequiresApi(api = Build.VERSION_CODES.P)
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        binding.imageView.setImageURI(result);
+                        Bitmap bitmap = null;
+                        ContentResolver contentResolver = getContentResolver();
+                        ImageDecoder.Source source = ImageDecoder.createSource(contentResolver, result);
+                        try {
+                            bitmap = ImageDecoder.decodeBitmap(source);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        photo = bitmap;
+                        file = result;
+                    }
+                });
+        PostSubmitButton(photo);
     }
     private void PostBackButton(){
         Button postBackButton = (Button) findViewById(R.id.back_button);
@@ -33,7 +87,7 @@ public class PostInformation extends AppCompatActivity {
             }
         });
     }
-    private void PostSubmitButton(){
+    private void PostSubmitButton(Bitmap picture){
         Button postSubmitButton = (Button) findViewById(R.id.submit_post_button);
         EditText description = (EditText) findViewById(R.id.description);
         EditText time = (EditText) findViewById(R.id.time);
@@ -49,11 +103,15 @@ public class PostInformation extends AppCompatActivity {
                 }
                 Intent intent = new Intent(PostInformation.this,GeneralFeed.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                int random_id = generateRandomId();
+                int user_id = 123; //this is an example, it should be generated from user login
+                pictures = storageRef.child(Integer.toString(user_id)+"/"+Integer.toString(random_id)+"/"+file.getLastPathSegment());
                 if(!TextUtils.isEmpty(description.getText().toString()) || !TextUtils.isEmpty(time.getText().toString())) {
                     if(choice)
-                        db.collection("feed").add(new post(123, description.getText().toString(), false, Integer.parseInt(time.getText().toString()), null));
+                        db.collection("feed").add(new post(user_id,random_id,description.getText().toString(), false, Integer.parseInt(time.getText().toString()), null));
                     else
-                        db.collection("feed").add(new voting_post(123, description.getText().toString(), false, Integer.parseInt(time.getText().toString()), null));
+                        db.collection("feed").add(new voting_post(user_id,random_id,description.getText().toString(), false, Integer.parseInt(time.getText().toString()), null));
+                    uploadTask = pictures.putFile(file);
                     startActivity(intent);
                 }
                 else {
@@ -63,5 +121,18 @@ public class PostInformation extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void UploadButton() {
+        ImageView picture = (ImageView) findViewById(R.id.imageView2);
+        ImageButton upload = (ImageButton) findViewById(R.id.upload_button);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTakePhoto.launch("image/*");
+            }
+        });
+    }
+    public int generateRandomId(){
+        return (int) (Math.random()*10000);
     }
 }
--- app/src/main/java/com/example/fitstagram/voting_post.java
+++ app/src/main/java/com/example/fitstagram/voting_post.java
@@ -5,19 +5,10 @@ import android.graphics.Bitmap;
 import java.util.ArrayList;
 
 public class voting_post extends post{
-
-    // A vote object will be stored in this arraylist, Integer as placeholder for now
-    // The vote object will contain:
-    // (int) user_id: user who casted the vote
-    // (int) vote: user's vote, integer between 1-3;
-    //      create castVote() function
-    // (boolean) correct: if the user chooses the most voted outfit
-    // (int) point_distribution(user_id): access and modify user's point count
-
     private ArrayList<Integer> total_votes;
 
-    public voting_post(int user_id, String description, boolean featured, int time, ArrayList<Bitmap> pictures) {
-        super(user_id, description, featured, time, pictures);
+    public voting_post(int user_id, int post_id, String description, boolean featured, int time, String[] pictures) {
+        super(user_id, post_id, description, featured, time, pictures);
         this.total_votes = total_votes;
     }
 
