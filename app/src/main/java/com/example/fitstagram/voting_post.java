b65c3164270bd9318ec62b6fb7fa0ef79286a6ea
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2022-04-23 20:07:25 -0500

    editted sign in and register xml to present nicer

--- app/src/main/java/com/example/fitstagram/voting_post.java
+++ app/src/main/java/com/example/fitstagram/voting_post.java
@@ -8,7 +8,7 @@ public class voting_post extends post{
 
     // A vote object will be stored in this arraylist, Integer as placeholder for now
     // The vote object will contain:
-    // (int) user_id: user who casted the vote
+    // (String) user_id: user who casted the vote
     // (int) vote: user's vote, integer between 1-3;
     //      create castVote() function
     // (boolean) correct: if the user chooses the most voted outfit
@@ -16,7 +16,7 @@ public class voting_post extends post{
 
     private ArrayList<Integer> total_votes;
 
-    public voting_post(int user_id, String description, boolean featured, boolean voting, int time, ArrayList<Bitmap> pictures, ArrayList<Integer> total_votes) {
+    public voting_post(String user_id, String description, boolean featured, boolean voting, int time, ArrayList<Bitmap> pictures, ArrayList<Integer> total_votes) {
         super(user_id, description, featured, voting, time, pictures);
         this.total_votes = total_votes;
     }
