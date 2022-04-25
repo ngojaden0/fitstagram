fe744a048d6cb2eaca1de26e417e758f91d8359c
ngojaden0 <ngojaden0@github.com>
2022-04-25 00:09:13 -0500

    added user and updated voting classes

--- app/src/main/java/com/example/fitstagram/vote.java
+++ app/src/main/java/com/example/fitstagram/vote.java
@@ -1,24 +1,14 @@
 package com.example.fitstagram;
 
-public class vote {
-    private int user_id;
+public class vote extends user{
     private int vote;
     private int point_distribution;
     private boolean correct;
 
     public vote(int user_id, int vote, int point_distribution, boolean correct) {
-        this.user_id = user_id;
         this.vote = vote;
         this.point_distribution = point_distribution;
         this.correct = correct;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
     }
 
     public int getVote() {
