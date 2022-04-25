651f61ae6e5038520fb098868e84911c2cc073ea
Christian <blundell0christian@gmail.com>
2022-04-25 18:27:40 -0500

    badge xml and redoing firestore ranking

--- app/src/main/java/com/example/fitstagram/ranking.java
+++ app/src/main/java/com/example/fitstagram/ranking.java
@@ -1,11 +1,14 @@
 package com.example.fitstagram;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,25 +18,73 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class ranking extends AppCompatActivity {
 
     RecyclerView recyclerview;
-    DatabaseReference database;
+    FirebaseFirestore db;
     RankingAdapter rAdapter;
-    ArrayList<userrank> list;
+    ArrayList<user> list;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking_list);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching data");
+        progressDialog.show();
 
+        recyclerview = findViewById(R.id.userlist);
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+        list = new ArrayList<user>();
+        rAdapter = new RankingAdapter(ranking.this, list);
+        recyclerview.setAdapter(rAdapter);
+
+        EventChangeListener();
 
         returnBtn();
         infoBtn();
+    }
+
+    private void EventChangeListener() {
+
+        db.collection("users").orderBy("total_points", Query.Direction.DESCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+
+                        if(e != null)
+                        {
+                            if(progressDialog.isShowing())
+                            {progressDialog.dismiss();}
+                            Log.e("Firestore error", e.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc : value.getDocumentChanges()){
+                            if(dc.getType() == DocumentChange.Type.ADDED){
+                                list.add(dc.getDocument().toObject(user.class));
+                            }
+                            rAdapter.notifyDataSetChanged();
+                            if(progressDialog.isShowing())
+                            {progressDialog.dismiss();}
+                        }
+                    }
+                });
     }
 
     private void returnBtn() {
